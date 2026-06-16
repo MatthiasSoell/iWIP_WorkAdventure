82b5bd3d9fbf4c03d8fdfd312c5267bf9f7f35f4
MatthiasSoell <matthias.soell@uni-rostock.de>
2026-06-16 14:13:03 +0200

    Initialisierung VS Code

---
+++ assets/iwip_logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="iwip_logo" tilewidth="141" tileheight="88" tilecount="1" columns="1">
+ <image source="iwip.png" width="141" height="88"/>
+</tileset>
